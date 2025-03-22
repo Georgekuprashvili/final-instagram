b529d8249dd0dd2e70ec9b3ae9b324a4f05de5bc
Georgekuprashvili <kuprashviligeorge@gmail.com>
2025-03-22 13:52:07 +0400

    Update Get_app.tsx

--- src/components/atoms__/get_app/Get_app.tsx
+++ src/components/atoms__/get_app/Get_app.tsx
@@ -5,49 +5,48 @@ import microsoft from "@/photos/microsoft.png";
 
 const Get_app = () => {
   return (
-    <>
-      <div className="flex  flex-col items-center justify-between gap-[10px]">
-        <div className="w-[350px] h-[60px] border-gray-500 border-[1px] flex justify-center items-center">
-          <p className="text-[white]">
-            Don't have an account?
-            <button className="text-[#259af6]">Sign up</button>
-          </p>
-        </div>
-
-        <p className="text-white">Get the app</p>
-
-        <div className="flex justify-center gap-[8px]">
-          <a
-            href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3D16ACAA17-CD59-4832-A9AD-EAC61214C066%26utm_campaign%3DunifiedHome%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge"
-            target="_blank"
-          >
-            <button className="bg-transparent p-0 border-none">
-              <Image
-                src={playstore}
-                alt="Get it on Google Play"
-                width={135}
-                height={40}
-                className="w-auto h-auto"
-              />
-            </button>
-          </a>
-          <a
-            href="https://apps.microsoft.com/detail/9nblggh5l9xt?hl=en-US&gl=US"
-            target="_blank"
-          >
-            <button className="bg-transparent p-0 border-none">
-              <Image
-                src={microsoft}
-                alt="Get it on Microsoft"
-                width={135}
-                height={40}
-                className="w-[150px] h-[60px]"
-              />
-            </button>
-          </a>
-        </div>
+    <div className="flex flex-col items-center justify-between gap-[10px]">
+      <div className="w-[350px] h-[60px] border-gray-500 border-[1px] flex justify-center items-center">
+        <p className="text-[white]">
+          Don&apos;t have an account?
+          <button className="text-[#259af6] ml-1">Sign up</button>
+        </p>
       </div>
-    </>
+
+      <p className="text-white">Get the app</p>
+
+      <div className="flex justify-center gap-[8px]">
+        <a
+          href="https://play.google.com/store/apps/details?id=com.instagram.android"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src={playstore}
+            alt="Get it on Google Play"
+            width={135}
+            height={40}
+            priority
+            className="w-auto h-auto"
+          />
+        </a>
+
+        <a
+          href="https://apps.microsoft.com/detail/9nblggh5l9xt?hl=en-US&gl=US"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="relative w-[150px] h-[60px]">
+            <Image
+              src={microsoft}
+              alt="Get it on Microsoft"
+              fill
+              className="object-contain"
+            />
+          </div>
+        </a>
+      </div>
+    </div>
   );
 };
 
